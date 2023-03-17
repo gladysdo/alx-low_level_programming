d350987431b4b885fafc52e22621c90fe6df6860
gladysdo <doe.glad@gmail.com>
2023-03-17 02:58:12 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+include < stdio.h >
+/**
+ * main - it is the main functions
+ *
+ * Return: it alway  return 0
+ **/
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle);
+	return (0);
+}
