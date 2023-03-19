7f0080e4c9231ca3cc29f800c73b3129de3cc180
gladysdo <doe.glad@gmail.com>
2023-03-19 13:58:56 -0700

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - the main function
  *
- * Return : l when successful
+ * Return: l always
  **/
 
 int main(void)
 {
-	write(2,"and that piece of art is useful" "- Dora Korpar, 2015-10-19\n",61)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61)
 	return (1);
 }
