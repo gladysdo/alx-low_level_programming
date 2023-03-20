8d87c4a38af6478085ecc6e581f8ac07e5dbfb24
gladysdo <doe.glad@gmail.com>
2023-03-20 09:27:07 -0700

    Hexadecimal-2

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int(i);
+	char(q);
+	for (i = 0; i <= 10; i++)
+		putchar(i + '0');
+	for (q = 'a'; q <= 'f'; q++)
+		putchar(q);
+	putchar('\n');
+	return (0);
+}
+
