185327fc3e28000927222c4c7b7b329e353eaa01
gladysdo <doe.glad@gmail.com>
2023-03-21 15:14:41 -0700

    function  alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function  print alphabet in lowercase
+ *
+ */
+
+void print_alphabet(void)
+{
+	char(k);
+	for (k = 'a'; k <= 'z'; k++)
+		_putchar(k);
+	_putchar('\n');
+}
