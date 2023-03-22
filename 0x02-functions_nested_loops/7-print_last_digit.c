27ba79b5ef638fdbefc2ca0097f0c4cbbc65afb4
gladysdo <doe.glad@gmail.com>
2023-03-22 06:12:01 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check code function
+ *
+ * @i: parameter function
+ *
+ * Return: k
+ */
+
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
+}
