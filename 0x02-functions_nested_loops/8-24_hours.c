37332074fd5fd22219eaea30ace7d030ceebf295
gladysdo <doe.glad@gmail.com>
2023-03-22 06:56:56 -0700

    hours 24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *jack_bauer - check function code print time
+ *
+ *Return: always 0
+ */
+
+void jack_bauer(void)
+{
+	int k;
+	int q;
+
+	for (k = 0; k <= 24; k++)
+	{
+		for (q = 0; q <= 60; q++)
+		{
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+			_putchar(':');
+			_putchar(q / 10 + '0');
+			_putchar(q % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
