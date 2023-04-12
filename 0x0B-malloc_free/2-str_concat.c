9c2ac3b3eea2b1c7bc7c45ffc7cf6b758e821aac
gladysdo <doe.glad@gmail.com>
2023-04-12 08:04:27 -0700

    pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,25 +1,37 @@
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
 
 
 char *str_concat(char *s1, char *s2)
 {
 	int i;
 	int j;
-	char *s
+	char  *concat;
 
 	if ( s1 == NULL)
+	{
 		s1 = "";
-
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
+	concat = (char *) malloc(s1 + s2 + 1) * sizeof(char);
 
-	s = malloc(ssize * sizeof(s1 + s2 +1);
-			if (s == NULL)
-			return (NULL);
-
-			if (i = 0; i < s; i++)
-			s = s1[i];
-
-			if (j = 0; j < s;)
-			s = s2[j];
-
+			if (concat == NULL)
+			{
+				return (NULL);
+			}
+			for (i = 0; i < *s1; i++)
+			{
+				concat[i] = s1[i];
+			}
+			for (j = 0; j < *s2; j++)
+			{
+				concat[i + j] = s2[j];
+			}
+			concat[i + j] = '\0';
+			return (concat);
+			return (0);
+}
