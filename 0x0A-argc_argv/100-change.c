451947bf02e71eed4a7aed7cca52d00c7eb47851
gladysdo <doe.glad@gmail.com>
2023-04-14 02:48:04 -0700

    minimum number of coins to make change for an amount of money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+	int i, amount, cents = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+		if (argc != 2)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		amount = atoi(argv[1]);
+		if (amount < 0)
+		{
+			printf("0\n");
+			return (0);
+		}
+		for (i = 0; i < 5; i++)
+		{
+			cents += amount / coins[i];
+			amount = amount % coins[i];
+		}
+		printf("%d\n", cents);
+		return (0);
+}
