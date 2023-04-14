6674449ac74ea60a6b7617ba60150b969cc486ba
gladysdo <doe.glad@gmail.com>
2023-04-14 02:58:27 -0700

    minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
-
-
+/**
+ * main - minimum number of coins to make change for an amount of money
+ * @argc: argument of count
+ * @argv: array of argument
+ * Return:0
+ */
 
 int main(int argc, char *argv[])
 {
