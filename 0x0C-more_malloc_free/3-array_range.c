296f58b0db1227d83b8ce13939ec2e5180af48ab
gladysdo <doe.glad@gmail.com>
2023-04-13 17:37:38 -0700

    allocates memory for an array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+int *array_range(int min, int max) {
+    int *arr = NULL;
+    int size, i;
+
+    // If min > max, return NULL
+    if (min > max) {
+        return NULL;
+    }
+
+    // Calculate the size of the array
+    size = max - min + 1;
+
+    // Allocate memory for the array
+    arr = malloc(size * sizeof(int));
+
+    // If malloc fails, return NULL
+    if (!arr) {
+        return NULL;
+    }
+
+    // Fill the array with values from min to max
+    for (i = 0; i < size; i++) {
+        arr[i] = min + i;
+    }
+
+    return arr;
+}
