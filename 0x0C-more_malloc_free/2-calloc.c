9a2ecdb66961980fc7b9138866828ffd9193b77b
gladysdo <doe.glad@gmail.com>
2023-04-13 17:25:49 -0700

    allocates memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *callocm = NULL;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	callocm = malloc(nmemb * size);
+	if (callocm)
+	{
+		memset(callocm, 0, nmemb * size);
+	}
+	if (!callocm)
+	{
+		return (NULL);
+	}
+	return (callocm);
+}
