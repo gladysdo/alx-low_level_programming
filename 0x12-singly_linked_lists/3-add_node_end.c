2458f0b34d45a7813add266dd86541ff6c48a59c
doe.glad@gmail.com <doe.glad@gmail.com>
2023-04-27 08:25:56 -0700

    printf function

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -17,7 +17,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
 	node = strdup(str);
+
 	if (str == NULL)
 	{
 		free(new);
@@ -28,6 +30,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->str = node;
 	new->len = len;
 	new->next = NULL;
+
 	if (*head == NULL)
 		*head = NULL;
 	else
